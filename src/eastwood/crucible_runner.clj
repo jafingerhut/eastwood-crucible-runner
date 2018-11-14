e2e2682e554e5d2a5a49aac40e47ea9cadbfa1e0
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-11-13 21:09:16 -0800

    More corrections from some basic testing

--- src/eastwood/crucible_runner.clj
+++ src/eastwood/crucible_runner.clj
@@ -4,32 +4,9 @@
             [clojure.string :as str]))
 
 
-(defn- accumulate [m k v]
-  (update-in m [k] (fnil conj #{}) v))
-
-
 (def cli-options
-  [
-   ["-d" "--dir DIRNAME" "Name of the directory containing tests. Defaults to \"test\"."
-    :parse-fn str
-    :assoc-fn accumulate]
-   ["-n" "--namespace SYMBOL" "Symbol indicating a specific namespace to test."
-    :parse-fn symbol
-    :assoc-fn accumulate]
-   ["-r" "--namespace-regex REGEX" "Regex for namespaces to test. Defaults to #\".*-test$\"\n                               (i.e, only namespaces ending in '-test' are evaluated)"
-    :parse-fn re-pattern
-    :assoc-fn accumulate]
-   ["-v" "--var SYMBOL" "Symbol indicating the fully qualified name of a specific test."
-    :parse-fn symbol
-    :assoc-fn accumulate]
-   ["-i" "--include KEYWORD" "Run only tests that have this metadata keyword."
-    :parse-fn parse-kw
-    :assoc-fn accumulate]
-   ["-e" "--exclude KEYWORD" "Exclude tests with this metadata keyword."
-    :parse-fn parse-kw
-    :assoc-fn accumulate]
-   ["-H" "--test-help" "Display this help message"]
-   ])
+  [["-v" "--versions FILENAME" "Data file describing combination of (Eastwood, Clojure, JDK) versions to run"]
+   ["-H" "--test-help" "Display this help message"]])
 
 
 (defn- help [args]
@@ -49,5 +26,4 @@
       (if (-> args :options :test-help)
         (help args)
         (let [opts (:options args)]
-          (println "opts=%s" opts)
-          (System/exit 0))))))
+          (println (format "opts=%s" opts)))))))
